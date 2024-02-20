e94e8351b9f088db797601ac8e24b5905ac64fa8
Seif Karoui <21332330+SeifKaroui@users.noreply.github.com>
2024-02-20 19:59:27 +0100

    first commit

---
+++ TCPClient.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+
+  public static void main(String[] args) {
+    try {
+      // Connect 
+      Socket socket = new Socket("localhost", 12345);
+      PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+      // Send message
+      String input = "Hello, world!";
+      writer.println(input);
+
+      // Receive response 
+      String response = reader.readLine();
+      System.out.println("Server response: " + response);
+
+    } catch (IOException e) {
+
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ TCPServer.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+public class TCPServer {
+
+  public static void main(String[] args) {
+    // Thread pool of 10
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
+    try {
+
+      ServerSocket serverSocket = new ServerSocket(12345);
+      System.out.println("Server Socket opened");
+      //
+      while (true) {
+        Socket socket = serverSocket.accept();
+
+        System.out.println("New connection");
+        // execute on new thread
+        executorService.submit(new ClientHandler(socket));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      executorService.shutdown();
+    }
+  }
+
+  private static class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+      this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+      try {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+        // Read message
+        String input = reader.readLine();
+
+        // Sleep
+        Thread.sleep(1000);
+
+        // Reverse message
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        // Send message
+        writer.println("Reversed: " + reversed);
+
+      } catch (IOException | InterruptedException e) {
+        e.printStackTrace();
+      } finally {
+        try {
+
+
+          socket.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+
+    }
+  }
+
+}
